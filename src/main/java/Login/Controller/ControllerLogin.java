3adde89aa7261475cd05aa72eac760cb6ca5c639
Razvan <mazilu.razvan.moisil@gmail.com>
2020-05-08 16:50:31 +0300

    FIS-67 Link login page with register page

--- src/main/java/Login/Controller/ControllerLogin.java
+++ src/main/java/Login/Controller/ControllerLogin.java
@@ -1,8 +1,16 @@
 package Login.Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 
 public class ControllerLogin {
@@ -14,6 +22,16 @@ public class ControllerLogin {
     private void handleClose() {
         Stage stage = (Stage) button.getScene().getWindow();
         stage.close();
+    }
+
+    @FXML
+    public void handleCreateAccount(javafx.event.ActionEvent event) throws IOException {
+        URL url = new File("src/main/resources/Register/Create Account Page.fxml").toURI().toURL(); //Login page
+        Parent home = FXMLLoader.load(url);
+        Scene scene = new Scene(home);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void handleButtonAction() {
