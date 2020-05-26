0d30652a5badff500b5be9a17e722afad9acfc75
Mateiu Adelina-Sonia <adelinasonia.mateiu@yahoo.com>
2020-05-26 16:33:25 +0300

    FIS-39 Create view books button and make view books default after login as a manager

--- src/main/java/Manager/Controllers/ControllerManager.java
+++ src/main/java/Manager/Controllers/ControllerManager.java
@@ -148,7 +148,12 @@ public class ControllerManager implements Initializable {
 
     @FXML
     public void BooksPage(javafx.event.ActionEvent event) throws IOException {
-
+        URL url = new File("src/main/resources/Manager/ManagerPage.fxml").toURI().toURL();
+        Parent home = FXMLLoader.load(url);
+        Scene scene = new Scene(home);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
