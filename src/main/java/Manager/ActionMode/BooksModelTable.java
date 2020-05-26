10246d805818792bae1fd7b81f59d83d6e5060eb
Mateiu Adelina-Sonia <adelinasonia.mateiu@yahoo.com>
2020-05-26 16:12:23 +0300

    FIS-72 Add book cover, details and pdf in tableView for Manager page

---
+++ src/main/java/Manager/ActionMode/BooksModelTable.java
@@ -0,0 +1,39 @@
+package Manager.ActionMode;
+import javafx.scene.control.Button;
+
+public class BooksModelTable {
+
+    private javafx.scene.image.ImageView picture;
+    private String information;
+    private Button pdf;
+
+    public BooksModelTable(javafx.scene.image.ImageView image, Book book) {
+
+        picture = image;
+        information = book.getTitle() + '\n' + book.getAuthor() + '\n' + book.getGenre() + '\n' + book.getDetails();
+    }
+
+    public javafx.scene.image.ImageView getPicture() {
+        return picture;
+    }
+
+    public void setPicture(javafx.scene.image.ImageView picture) {
+        this.picture = picture;
+    }
+
+    public String getInformation() {
+        return information;
+    }
+
+    public void setInformation(String information) {
+        this.information = information;
+    }
+
+    public Button getPdf() {
+        return pdf;
+    }
+
+    public void setPdf(Button pdf) {
+        this.pdf = pdf;
+    }
+}
