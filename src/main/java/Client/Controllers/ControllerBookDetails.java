efb951726cd3ad6ea0e557c777d706fa6c5e1464
Mateiu Adelina-Sonia <adelinasonia.mateiu@yahoo.com>
2020-06-03 11:28:08 +0300

    FIS-58 Create add request button with minimal functionality

--- src/main/java/Client/Controllers/ControllerBookDetails.java
+++ src/main/java/Client/Controllers/ControllerBookDetails.java
@@ -1,5 +1,7 @@
 package Client.Controllers;
 
+import Client.Services.AddRequest;
+import Manager.Services.AddJSON;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -7,13 +9,19 @@ import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.Base64;
+import java.util.List;
 
 import static Client.Controllers.ControllerBooks.bookSave;
+import static Login.controllers.ControllerLogin.saveUser;
+import static Login.controllers.ControllerLogin.userSave;
 
 
 public class ControllerBookDetails {
@@ -26,6 +34,22 @@ public class ControllerBookDetails {
 
     @FXML
     private ImageView bookCover;
+
+    @FXML
+    private Label sent;
+
+    @FXML
+    private Label ten;
+
+    @FXML
+    private Label exists;
+
+    @FXML
+    public void handleRequest() {
+            //if (userSave != null)
+               // AddRequest.addRequest(userSave, bookSave);
+            sent.setText("Request successfully sent!");
+    }
 
     @FXML
     private void handleClose() {
