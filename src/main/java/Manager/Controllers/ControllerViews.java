1e957d76647fbda1f4a6475146fe9ad6fa1646d0
Razvan <mazilu.razvan.moisil@gmail.com>
2020-06-09 10:55:56 +0300

    FIS-49 Create Views button in Views window

--- src/main/java/Manager/Controllers/ControllerViews.java
+++ src/main/java/Manager/Controllers/ControllerViews.java
@@ -57,7 +57,12 @@ public class ControllerViews {
     }
 
     @FXML
-    public void handleViews(ActionEvent event) {
-
+    public void handleViews(ActionEvent event) throws IOException {
+        URL url = new File("src/main/resources/Manager/ViewsPage.fxml").toURI().toURL();
+        Parent home = FXMLLoader.load(url);
+        Scene scene = new Scene(home);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 }
