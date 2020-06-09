17ead479fa44f1e42865cadfef12f7ff3d5dc90f
Razvan <mazilu.razvan.moisil@gmail.com>
2020-06-09 10:57:27 +0300

    FIS-49 Update Manager page, Request page and their controllers for the views button

--- src/main/java/Manager/Controllers/ControllerManager.java
+++ src/main/java/Manager/Controllers/ControllerManager.java
@@ -7,6 +7,7 @@ import Manager.Services.AddJSON;
 import Manager.Services.ReadJSON;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -202,6 +203,16 @@ public class ControllerManager implements Initializable {
     public void handleRequest(javafx.event.ActionEvent event) throws IOException {
         AddRequest.loadRequestsFromFile();
         URL url = new File("src/main/resources/Manager/RequestPage.fxml").toURI().toURL();
+        Parent home = FXMLLoader.load(url);
+        Scene scene = new Scene(home);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void handleViews(ActionEvent event) throws IOException {
+        URL url = new File("src/main/resources/Manager/ViewsPage.fxml").toURI().toURL();
         Parent home = FXMLLoader.load(url);
         Scene scene = new Scene(home);
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
