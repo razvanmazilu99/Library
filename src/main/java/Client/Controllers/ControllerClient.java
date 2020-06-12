fa01a6457f91388a259a65fb978690c2fed6574d
Mateiu Adelina-Sonia <adelinasonia.mateiu@yahoo.com>
2020-06-12 12:02:07 +0300

    FIS-51 Update buttons for client requests

--- src/main/java/Client/Controllers/ControllerClient.java
+++ src/main/java/Client/Controllers/ControllerClient.java
@@ -144,4 +144,14 @@ public class ControllerClient implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    public void RequestPage(javafx.event.ActionEvent event) throws IOException {
+        URL url = new File("src/main/resources/Client/ClientRequestPage.fxml").toURI().toURL();
+        Parent home = FXMLLoader.load(url);
+        Scene scene = new Scene(home);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
